321f31be8e15e34763d91010625aea9359b7a5be
xxx-Event <s2119@e-mirim.hs.kr>
2023-04-17 12:01:20 +0900

    Thread.sleep() 연습

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp
@@ -10,6 +11,13 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.Write("메소드 호출 1");
+            Thread.Sleep(1000);
+            Console.SetCursorPosition(5, 5);
+            Console.Write("메서드 호출 2");
+            Thread.Sleep(2000);
+            Console.SetCursorPosition(0, 0);
+            Console.Write("메서드 호출 3");
         }
     }
 }
